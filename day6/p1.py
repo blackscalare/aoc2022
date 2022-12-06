17c710ea83d9e93700a9860d688c70fba4749ab2
blackscalare <blackscalare@gmail.com>
2022-12-06 16:36:45 +0100

    refactored

--- day6/p1.py
+++ day6/p1.py
@@ -1,12 +1,6 @@
 line = open('input.txt').readline().rstrip()
 
-idx = 0
 for i in range(len(line)):
-    d = set()
-
-    [d.add(line[x]) for x in range(idx, idx+4)]
-
-    if(len(d) == 4):
+    if len(set(line[i:i+4])) == 4:
         print(i + 4)
         exit()
-    idx += 1
--- day6/p2.py
+++ day6/p2.py
@@ -1,12 +1,6 @@
 line = open('input.txt').readline().rstrip()
 
-idx = 0
 for i in range(len(line)):
-    d = set()
-
-    [d.add(line[x]) for x in range(idx, idx+14)]
-
-    if(len(d) == 14):
+    if len(set(line[i:i+14])) == 14:
         print(i + 14)
         exit()
-    idx += 1
