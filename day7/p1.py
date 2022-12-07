9d9497b99fcd82a2893e041347a12d2e093498e3
blackscalare <blackscalare@gmail.com>
2022-12-07 22:56:46 +0100

    day7

--- day7/p1.py
+++ day7/p1.py
@@ -1,69 +1,46 @@
-from collections import defaultdict
-from anytree import Node
+#!/bin/python3
 with open('input.txt') as f:
-    lines = [x.rstrip() for x in f]
+    lines = [x.rstrip()for x in f]
 
-EXEC = '$'
-CD = 'cd'
-LS = 'ls'
-ls_mode = False
-nodes = {}
-current_dir = ''
-last_dir = ''
-current_node = None
-last_node = ''
-for line in lines:
-    line = line.split(' ')
-    op = line[0]
+fs = {'size': 0}
+cwd = []
 
-    if op == EXEC or op == CD:
-        ls_mode = False
-    
-    if ls_mode:
-        if line[0].isnumeric():
-            nodes[current_dir][current_node] += int(line[0])
-        else:
-            pass
-
-    if op == EXEC:
-        if line[1] == CD:
-            last_dir = current_dir
-            last_node = current_node
-            d = line[2]
-            if d == '..':
-                nodes[last_dir]
-            elif d == '/':
-                current_dir = d
-                if d not in nodes.keys():
-                    nodes[d] = {Node(d): 0}
-                current_node = list(nodes[d])[0]
-            else:
-                if d not in nodes.keys():
-                    nodes[d] = {Node(d, parent=list(nodes[last_dir])[0]): 0}
-                    current_dir = d
-                    current_node = list(nodes[d])[0]
+def populate(lines):
+    for line in lines:
+        line = line.split()
+        if line[0] == '$':
+            if line[1] == 'cd':
+                if line[2] == '..':
+                    cwd.pop()
                 else:
-                    names = [x.name for x in list(nodes[d])[0].children]
-                    if d in names:
-                        current_dir = d
-                        current_node = list(nodes[d])[0]
-        elif line[1] == LS:
-            ls_mode = True
+                    cwd.append(line[2])
+        else:
+            current = fs
+            for dir in cwd:
+                current = current[dir]
+            if line[0] == 'dir':
+                if line[1] not in current.keys():
+                    current[line[1]] = {}
+            else:
+                value = int(line[0])
+                fs['size'] += value
+                current = fs
+                for dir in cwd:
+                    current = current[dir]
+                    if 'size' in current:
+                        current['size'] += value
+                    else:
+                        current['size'] = value
 
-print(nodes)
-for l in nodes.keys():
-    #print(list(nodes[l].keys())[0].parent, nodes[l])
-    node = list(nodes[l].keys())[0]
-    parent = list(nodes[l].keys())[0].parent
-    if parent is not None:
-        value = nodes[node.name][node] + nodes[parent.name][parent]
-        if value <= 100000:
-            nodes[parent.name] = {parent: value}
-tot = 0
-for l in nodes.keys():
-    key = list(nodes[l].keys())[0]
-    val = nodes[l][key]
-    if val <= 100000:
-        tot += nodes[l][key]
-    #tot += list(nodes[l].keys())[0]
-print(tot)
+def get_sizes(fs, sizes):
+    for key in fs.keys():
+        if type(fs[key]) is dict:
+            get_sizes(fs[key], sizes)
+    sizes.append(fs['size'])
+
+populate(lines[1:])
+
+sizes = []
+get_sizes(fs, sizes)
+s = sum([int(x) for x in sizes if int(x) <= 100000])
+print(s)
--- day7/p2.py
+++ day7/p2.py
@@ -1,3 +1,49 @@
+#!/bin/python3
 with open('input.txt') as f:
-    lines = [x.rstrip() for x in f]
+    lines = [x.rstrip()for x in f]
 
+fs = {'size': 0}
+cwd = []
+
+def populate(lines):
+    for line in lines:
+        line = line.split()
+        if line[0] == '$':
+            if line[1] == 'cd':
+                if line[2] == '..':
+                    cwd.pop()
+                else:
+                    cwd.append(line[2])
+        else:
+            current = fs
+            for dir in cwd:
+                current = current[dir]
+            if line[0] == 'dir':
+                if line[1] not in current.keys():
+                    current[line[1]] = {}
+            else:
+                value = int(line[0])
+                fs['size'] += value
+                current = fs
+                for dir in cwd:
+                    current = current[dir]
+                    if 'size' in current:
+                        current['size'] += value
+                    else:
+                        current['size'] = value
+
+def get_sizes(fs, sizes):
+    for key in fs.keys():
+        if type(fs[key]) is dict:
+            get_sizes(fs[key], sizes)
+    sizes.append(fs['size'])
+
+populate(lines[1:])
+
+sizes = []
+get_sizes(fs, sizes)
+
+minimum_to_delete = 30000000 - (70000000 - fs['size'])
+
+s = sorted([int(x) for x in sizes if int(x) >= minimum_to_delete])[0]
+print(s)
